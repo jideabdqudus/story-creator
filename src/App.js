044357b7b4c4ce67077a676274957415af8f921a
jideabdqudus <jideabdqudus@gmail.com>
2020-05-22 13:08:28 +0100

    Added Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import NavbarCard from './components/Navbar/NavbarCard';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavbarCard/>
     </div>
   );
 }
