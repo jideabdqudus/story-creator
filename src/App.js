965048240953c6bdbfa63519385a34e92f405c55
jideabdqudus <jideabdqudus@gmail.com>
2020-05-23 11:48:09 +0100

    Added Changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavbarCard from './components/Navbar/NavbarCard';
 import HeaderCard from './components/Header/HeaderCard';
 import FormCard from './components/Form/FormCard';
 import DashTable from './components/Dashboard/DashTable';
+import StoriesCard from './components/Stories/StoriesCard';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <HeaderCard/>
       <FormCard/>
       <DashTable/>
+      <StoriesCard/>
     </div>
   );
 }
